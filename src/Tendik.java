3f1bd30e1307fefed699d6a173b2b73e75db01e5
adhikanugraha <67616777+adhikanugraha@users.noreply.github.com>
2022-11-08 10:06:01 +0700

    Update Tendik.java

--- src/Tendik.java
+++ src/Tendik.java
@@ -8,8 +8,8 @@ public class Tendik extends Karyawan{
     public Tendik(String nama, String alamat, String tanggal_lahir, int tahun_masuk, String jenis_kelamin, String nip) {
         super(nama, alamat, tanggal_lahir, tahun_masuk, jenis_kelamin, nip);
     }
-    public void lembur(int hari_lembur){
-        this.gaji_tambahan = hari_lembur*50000;
+    public void lembur(int jam_lembur){
+        this.gaji_tambahan = jam_lembur*50000;
     }
     public int getGaji_tambahan() {
         return gaji_tambahan;
